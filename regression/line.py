432fb0d03ffd4f45937de37f7c4d87b2fb7b38d9
cza <chenziangsg@163.com>
2019-11-17 19:06:57 +0800

    update machine learning code

---
+++ regression/line.py
@@ -0,0 +1,98 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import random
+from sklearn.linear_model import LinearRegression
+
+# y = kx + b
+learningRate = 0.0001
+k = b = 0
+maxIter = 50
+
+# 最小二乘法
+def computeError(dataSetX, dataSetY, k, b, baseError=0):
+    m = len(dataSetX)
+    for i in range(m):
+        baseError += (dataSetY[i] - (k * dataSetX[i] + b)) ** 2
+    return baseError / (2 * float(m))
+
+
+def compute(dataSetX, dataSetY, k, b, learningRate, maxIter):
+    m = float(len(dataSetX))
+    for i in range(maxIter):
+        k_t = b_t = 0
+        for j in range(int(m)):
+            k_t += (1 / m) * ((k * dataSetX[j] + b) - dataSetY[j]) * dataSetX[j]
+            b_t += (1 / m) * ((k * dataSetX[j] + b) - dataSetY[j])
+        k = k - learningRate * k_t
+        b = b - learningRate * b_t
+    return k, b
+
+
+def mulComputeError(dataSetX1, dataSetX2, dataSetY, k1, k2, b, baseError=0):
+    m = len(dataSetX1)
+    for i in range(m):
+        baseError += (dataSetY[i] - (k1 * dataSetX1[i] + k2 * dataSetX2[i] + b)) ** 2
+    return baseError / (2 * float(m))
+
+
+def mulCompute(dataSetX1, dataSetX2, dataSetY, k1, k2, b, learningRate, maxIter):
+    m = float(len(dataSetX1))
+    for i in range(maxIter):
+        k1_t = k2_t = b_t = 0
+        for j in range(int(m)):
+            k1_t += (1 / m) * ((k1 * dataSetX1[j] + k2 * dataSetX2[j] + b) - dataSetY[j]) * dataSetX1[j]
+            k2_t += (1 / m) * ((k1 * dataSetX1[j] + k2 * dataSetX2[j] + b) - dataSetY[j]) * dataSetX2[j]
+            b_t += (1 / m) * ((k1 * dataSetX1[j] + k2 * dataSetX2[j] + b) - dataSetY[j])
+        k1 = k1 - learningRate * k1_t
+        k2 = k2 - learningRate * k2_t
+        b = b - learningRate * b_t
+    return k1, k2, b
+
+
+
+
+def weight(xArr, yArr):
+    xMat = np.mat(xArr)
+    yMat = np.mat(yArr)
+    xTx = xMat.T * xMat  # 矩阵乘法
+    if np.linalg.det(xTx) == 0.0:  # 值为0表示没有逆矩阵
+        return
+    ws = xTx.I * xMat.T * yMat  # .I就是逆矩阵的意思，这里就是直接套公式了
+    return ws
+
+
+
+if __name__ == '__main__':
+    dataSetX = [random.randint(i, i + 10) for i in range(100)]
+    dataSetY = [random.randint(i, i + 10) for i in range(100)]
+    # k, b = compute(dataSetX, dataSetY, k, b, learningRate, maxIter)
+    # plt.scatter(dataSetX, dataSetY)
+    # plt.plot(dataSetX, dataSetY, 'b.')
+    # plt.plot(np.array(dataSetX), k * np.array(dataSetX) + b, 'r')
+    # plt.show()
+    # print(k, b)
+
+    # k1 = k2 = b = 0
+    # dataSetX1 = [100, 50, 100, 100, 50, 80, 75, 65, 90, 90]
+    # dataSetX2 = [4, 3, 4, 2, 2, 2, 3, 4, 3, 2]
+    # dataSetY = [9.3, 4.8, 8.9, 6.5, 4.2, 6.2, 7.4, 6.0, 7.6, 6.1]
+    # print(mulComputeError(dataSetX1, dataSetX2, dataSetY, k1, k2, b))
+    # k1, k2, b = mulCompute(dataSetX1, dataSetX2, dataSetY, k1, k2, b, learningRate, maxIter)
+    # print(mulComputeError(dataSetX1, dataSetX2, dataSetY, k1, k2, b))
+
+    # 标准方程法
+    x_data = np.array([dataSetX]).T
+    X_data = np.concatenate((np.ones((100, 1)), x_data), axis=1)
+    Y_data = np.array([dataSetY]).T
+    ws = weight(X_data, Y_data)
+    print(ws, ws[0], ws[1])
+    plt.plot(dataSetX, dataSetY, 'b.')
+    plt.plot(np.array([[0], [100]]),  np.array([[0], [100]])*ws[1] + ws[0], 'r')
+    plt.show()
+
+
+"""
+100, 50, 100, 100, 50, 80, 75, 65, 90, 90
+4, 3, 4, 2, 2, 2, 3, 4, 3, 2
+9.3, 4.8, 8.9, 6.5, 4.2, 6.2, 7.4, 6.0, 7.6, 6.1
+"""
--- svm/smo.py
+++ svm/smo.py
@@ -18,32 +18,42 @@ def clipAlpha(aj, H, L):
 
 
 def smoSimple(dataMatIn, classLabels, C, toler, maxIter):
-    dataMatrix = np.mat(dataMatIn)
-    labelMat = np.mat(classLabels).transpose()
+    """
+
+    :param dataMatIn: 数据集
+    :param classLabels: 类别标签
+    :param C: 常熟
+    :param toler: 容错率
+    :param maxIter: 最大循环次数
+    :return:
+    """
+    dataMatrix = np.mat(dataMatIn)  # 将数据集转化为矩阵
+    labelMat = np.mat(classLabels).transpose()  # 将标签转置
     b = 0
-    m, n = dataMatrix.shape
-    alphas = np.mat(np.zeros((m, 1)))
-    iter = 0
+    m, n = dataMatrix.shape  # 矩阵的shape
+    alphas = np.mat(np.zeros((m, 1)))  # 创建m行1列的0矩阵
+    iter = 0  # iter是循环次数的意思吗
     while (iter < maxIter):
-        alphaPairsChanged = 0
+        alphaPairsChanged = 0  # 用于记录alphas是否已经进行优化
         for i in range(m):
-            fXi = float(np.multiply(alphas, labelMat).T * (dataMatrix * dataMatrix[i, :].T)) + b
+            fXi = float(np.multiply(alphas, labelMat).T * (dataMatrix * dataMatrix[i, :].T)) + b  # 我们预测的类别
             Ei = fXi - float(labelMat[i])
             if ((labelMat[i] * Ei) < -toler) and (alphas[i] < C) or ((labelMat[i] * Ei > toler) and (alphas[i] > 0)):
-                j = selectJrand(i, m)
+                j = selectJrand(i, m)  # 随机选择第二个alpha
                 fXj = float(np.multiply(alphas, labelMat).T * (dataMatrix * dataMatrix[j, :].T)) + b
                 Ej = fXj - float(labelMat[j])
                 alphaIold = alphas[i].copy()
                 alphaJold = alphas[j].copy()
-                if (labelMat[i] != labelMat[j]):
-                    L = max(0, alphas[j] - alphas[i])
+                if (labelMat[i] != labelMat[j]):  # if-else 用于保证alpha在0-C之间
+                    L = max(0, alphas[j] - alphas[i])  # L和H用于将alpha[j]调整到0和C之间
                     H = min(C, C + alphas[j] - alphas[i])
                 else:
                     L = max(0, alphas[j] + alphas[i] - C)
                     H = min(C, alphas[j] + alphas[i])
-                if L == H:
+                if L == H:  # 若LH相等，则不做处理
                     print("L == H")
                     continue
+                # eta表示alpha[j]的最优修改量
                 eta = 2.0 * dataMatrix[i, :] * dataMatrix[j, :].T - \
                       dataMatrix[i, :] * dataMatrix[i, :].T - dataMatrix[j, :] * dataMatrix[j, :].T
                 if eta >= 0:
@@ -54,6 +64,7 @@ def smoSimple(dataMatIn, classLabels, C, toler, maxIter):
                 if (abs(alphas[j] - alphaJold) < 0.00001):
                     print("J not moving enough")
                     continue
+                # 对i进行修改，修改量与j相同，但方向相反
                 alphas[i] += labelMat[j] * labelMat[i] * (alphaJold - alphas[j])
                 b1 = b - Ei - labelMat[i] * (alphas[i] - alphaIold) * dataMatrix[i, :] * dataMatrix[i, :].T - labelMat[
                     j] * (alphas[j] - alphaJold) * dataMatrix[i, :] * dataMatrix[j, :].T
@@ -77,8 +88,20 @@ def smoSimple(dataMatIn, classLabels, C, toler, maxIter):
 
 if __name__ == '__main__':
     # dataS = [[1, 0], [1, 0], [1, 0], [1, 0], [1, 0], [1, 0], [1, 0], [0, 1], [0, 1], [0, 1]]
-    dataS = [[0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [1, 1], [1, 1], [1, 1]]
-    label = [-1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, 1.0, 1.0, 1.0]
-    b, alphas = smoSimple(dataS, label, 0.6, 0.001, 40)
-    print(b)
-    print(alphas)
+    # dataS = [[0, -1], [0, -1], [0, -1], [0, -1], [0, -1], [0, -1], [0, 0], [1, 2], [1, 2], [1, 1]]
+    # label = [-1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, 1.0, 1.0, 1.0]
+    # b, alphas = smoSimple(dataS, label, 0.6, 0.001, 40)
+    # # print(b)
+    # # print(alphas)
+    # for i in range(len(label)):
+    #     if alphas[i] > 0:
+    #         print(dataS[i], label[i])
+
+    a = np.mat(np.array([1,2,3,4,5]))
+    b = np.mat(np.array([1,2]))
+    print(a)
+    print(b.T)
+    # print(a*b.T)
+    # print(np.multiply(a,b))
+    print(np.multiply(a,b.T))
+    print(np.multiply(b.T, a))
