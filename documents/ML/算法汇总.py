2bb5c9abaa887197a6c46475bc02421fc4af7513
cza <czasg0.0>
2019-11-18 17:39:48 +0800

    update

---
+++ documents/ML/算法汇总.py
@@ -0,0 +1,16 @@
+__file__ = "Machine Learning"
+
+"""
+1、KNN
+类别：二分类/多分类
+
+2、DTree
+类别：多分类
+说明：一种类权重分配算法
+
+3、NaiveBayes
+类别：二分类/多分类
+
+
+
+"""  # todo 目前新闻分类可以采用 KNN和NaiveBayes，但是NaiveBayes对数据训练有一定算法要求
